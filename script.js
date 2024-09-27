0cf849b04fef847bade65cc54dd145bce0bae4df
rayssa garcia <rayssagarcia035@gmail.com>
2024-09-27 10:46:42 -0300

    clear

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function inserir(num) {
-    let numero = document.getElementById('res').innerHTML
-    innerHTML
+    let numer = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = numer + num
+  
+}
+
+function clean() {
+   //document.getElementById('res').innerHTML=''
+    let limpar = document.getElementById('res')
+    res.innerHTML= ''
+
 }
 
