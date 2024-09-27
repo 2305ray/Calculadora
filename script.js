5fe4d8ef3d2626a33fc9f00daf59f93752db267f
rayssa garcia <rayssagarcia035@gmail.com>
2024-09-27 11:26:45 -0300

    concluído

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function clean() {
 
 }
 
+function back () {
+    let res = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = res.substring(0, res.length -1)
+}
+
+function calcular() {
+    let res = document.getElementById('res').innerHTML
+         if (res) {
+          document.getElementById('res').innerHTML = eval(res)  
+         } else {
+            document.getElementById('res').innerHTML = "Nada..."
+         }
+}
